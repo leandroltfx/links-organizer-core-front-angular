b03419cf30ee6cc9981ab09190c9d3e7d05fcc7e
leandroltfx <leandroltfbr@hotmail.com>
2025-07-15 20:18:23 -0300

    feat(user-registration): iniciando módulo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "cadastro",
+        loadComponent: () => import('./modules/user-registration/user-registration.component').then(m => m.UserRegistrationComponent)
+    }
+];
