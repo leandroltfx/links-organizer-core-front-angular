e38bf49c694d288212c06e36f2af2de69285f6e7
leandroltfx <leandroltfbr@hotmail.com>
2025-07-15 08:49:58 -0300

    configurando lib de componentes ng-zorro

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,26 @@
+import pt from '@angular/common/locales/pt';
+import { FormsModule } from '@angular/forms';
 import { provideRouter } from '@angular/router';
-import { ApplicationConfig } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import { provideHttpClient } from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import { pt_BR, provideNzI18n } from 'ng-zorro-antd/i18n';
 
 import { routes } from './app.routes';
 
+registerLocaleData(pt);
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideHttpClient(),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAnimationsAsync(),
+    importProvidersFrom(FormsModule),
+
+    provideNzI18n(pt_BR),
+  ]
 };
