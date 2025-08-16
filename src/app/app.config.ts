f2bff8f34ec843fff594b669b7828a3a3b1c818e
leandroltfx <leandroltfbr@hotmail.com>
2025-08-16 14:26:51 -0300

    organiza imports

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,26 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
-import { pt_BR, provideNzI18n } from 'ng-zorro-antd/i18n';
-import { registerLocaleData } from '@angular/common';
 import pt from '@angular/common/locales/pt';
 import { FormsModule } from '@angular/forms';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter } from '@angular/router';
+import { registerLocaleData } from '@angular/common';
 import { provideHttpClient } from '@angular/common/http';
+import { provideClientHydration } from '@angular/platform-browser';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import { pt_BR, provideNzI18n } from 'ng-zorro-antd/i18n';
+
+import { routes } from './app.routes';
 
 registerLocaleData(pt);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideNzI18n(pt_BR), importProvidersFrom(FormsModule), provideAnimationsAsync(), provideHttpClient()]
+  providers: [
+    provideHttpClient(),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAnimationsAsync(),
+    importProvidersFrom(FormsModule),
+    
+    provideNzI18n(pt_BR)
+  ]
 };
