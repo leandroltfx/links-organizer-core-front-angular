833073c1377f53fa562fad06a12997e7eb00e858
leandroltfx <leandroltfbr@hotmail.com>
2025-08-16 14:35:01 -0300

    cria componente para tela de login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        loadComponent: () => import('./modules/login/login.component').then(m => m.LoginComponent)
+    },
+    {
+        path: '**',
+        redirectTo: 'login'
+    }
+];
