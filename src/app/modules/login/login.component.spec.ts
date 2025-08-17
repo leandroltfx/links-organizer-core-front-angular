d642f67ba7c99983a447d797da9d8b10cb01e6dc
leandroltfx <leandroltfbr@hotmail.com>
2025-08-17 18:48:11 -0300

    cria testes unitários nos componentes de login e cadastro de usuário

--- src/app/modules/login/login.component.spec.ts
+++ src/app/modules/login/login.component.spec.ts
@@ -1,3 +1,4 @@
+import { Router, RouterModule } from '@angular/router';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
@@ -6,10 +7,13 @@ import { NgZorroModules } from '../../shared/ng-zorro-modules';
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
+        RouterModule,
+
         LoginComponent,
         NgZorroModules
       ]
@@ -18,10 +22,34 @@ describe('LoginComponent', () => {
     
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('ngOnInit', () => {
+
+    it('deve iniciar o componente construíndo o formulário de login', () => {
+
+      component.ngOnInit();
+
+      expect(Object.keys(component.loginForm.controls).length).toBe(2);
+      for (const key in component.loginForm.controls) {
+        expect(key === 'email' || key === 'password');
+      }
+    });
+
+  });
+
+  describe('goToUserRegistration', () => {
+
+    it('deve chamar a rota de cadastro de usuário', () => {
+
+      const navigateSpy = spyOn(router, 'navigate');
+
+      component.goToUserRegistration();
+
+      expect(navigateSpy).toHaveBeenCalledWith(['/cadastro']);
+    });
+
   });
 });
--- src/app/modules/user-registration/user-registration.component.spec.ts
+++ src/app/modules/user-registration/user-registration.component.spec.ts
@@ -1,23 +1,55 @@
+import { Router, RouterModule } from '@angular/router';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { NgZorroModules } from '../../shared/ng-zorro-modules';
 import { UserRegistrationComponent } from './user-registration.component';
 
 describe('UserRegistrationComponent', () => {
   let component: UserRegistrationComponent;
   let fixture: ComponentFixture<UserRegistrationComponent>;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [UserRegistrationComponent]
+      imports: [
+        RouterModule,
+
+        NgZorroModules,
+        UserRegistrationComponent
+      ]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(UserRegistrationComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('ngOnInit', () => {
+
+    it('deve iniciar o componente construíndo o formulário de cadastro de usuário', () => {
+
+      component.ngOnInit();
+
+      expect(Object.keys(component.userRegistrationForm.controls).length).toBe(3);
+      for (const key in component.userRegistrationForm.controls) {
+        expect(key === 'email' || key === 'password' || key === 'userName');
+      }
+    });
+
+  });
+
+  describe('goToLogin', () => {
+
+    it('deve chamar a rota de login', () => {
+
+      const navigateSpy = spyOn(router, 'navigate');
+
+      component.goToLogin();
+
+      expect(navigateSpy).toHaveBeenCalledWith(['/login']);
+    });
+
   });
 });
