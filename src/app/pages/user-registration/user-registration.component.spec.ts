85eccf8907273eb6fe65e9055c5668a48b0cfbc7
leandroltfx <leandroltfbr@hotmail.com>
2025-09-06 08:33:37 -0300

    cria formulário de cadastro de usuário

--- src/app/pages/user-registration/user-registration.component.spec.ts
+++ src/app/pages/user-registration/user-registration.component.spec.ts
@@ -1,4 +1,12 @@
+import { ReactiveFormsModule } from '@angular/forms';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { UserRegistrationComponent } from './user-registration.component';
 
@@ -8,10 +16,21 @@ describe('UserRegistrationComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [UserRegistrationComponent]
+      imports: [
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+
+        MatCardModule,
+        MatIconModule,
+        MatInputModule,
+        MatButtonModule,
+        MatFormFieldModule,
+
+        UserRegistrationComponent,
+      ]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(UserRegistrationComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/pages/user-registration/user-registration.component.ts
+++ src/app/pages/user-registration/user-registration.component.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'lo-user-registration',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+
+    MatCardModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+  ],
   templateUrl: './user-registration.component.html',
   styleUrl: './user-registration.component.css'
 })
-export class UserRegistrationComponent {
+export class UserRegistrationComponent implements OnInit {
+
+  public userRegistrationForm!: FormGroup;
+  public hidePassword: boolean = true;
+
+  constructor(
+    private readonly router: Router,
+    private readonly formBuilder: FormBuilder,
+  ) { }
+
+  public ngOnInit(): void {
+    this.userRegistrationForm = this.buildUserRegistrationForm();
+  }
+
+  public register(): void { }
+
+  public cancel(): void {
+    this.router.navigate(['/login']);
+  }
+
+  private buildUserRegistrationForm(): FormGroup {
+    return this.formBuilder.group(
+      {
+        userName: '',
+        email: '',
+        password: '',
+      }
+    );
+  }
 
 }
