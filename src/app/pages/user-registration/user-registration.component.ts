b6be03b249a594c80bd4d64229bdd6911bcf8ae1
leandroltfx <leandroltfbr@hotmail.com>
2025-09-06 10:38:09 -0300

    adiciona validações nos campos do formulário de cadastro de usuário

--- src/app/pages/user-registration/user-registration.component.ts
+++ src/app/pages/user-registration/user-registration.component.ts
@@ -1,6 +1,6 @@
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
@@ -26,7 +26,16 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 export class UserRegistrationComponent implements OnInit {
 
   public userRegistrationForm!: FormGroup;
+
   public hidePassword: boolean = true;
+
+  public messagePatternErrorPassword: string = 'A senha deve ter entre 8 e 80 caracteres.';
+  public messagePatternErrorEmail: string = 'Digite um e-mail válido no formato "exemplo@dominio.com" sem ultrapassar 254 caracteres.';
+  public messagePatternErrorUserName: string = 'O nome de usuário deve ter entre 3 e 30 caracteres, começar com uma letra e conter apenas letras e números (sem espaços ou símbolos).';
+
+  private patternPassword: RegExp = /^.{8,80}$/;
+  private patternUserName: RegExp = /^[A-Za-z][A-Za-z0-9]{2,29}$/;
+  private patternEmail: RegExp = /^(?=.{1,254}$)[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
   constructor(
     private readonly router: Router,
@@ -46,9 +55,9 @@ export class UserRegistrationComponent implements OnInit {
   private buildUserRegistrationForm(): FormGroup {
     return this.formBuilder.group(
       {
-        userName: '',
-        email: '',
-        password: '',
+        email: ['', [Validators.required, Validators.pattern(this.patternEmail)]],
+        userName: ['', [Validators.required, Validators.pattern(this.patternUserName)]],
+        password: ['', [Validators.required, Validators.pattern(this.patternPassword)]],
       }
     );
   }
