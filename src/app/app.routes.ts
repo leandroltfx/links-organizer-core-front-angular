09d1b25d669ab10f442fde6c8833682d38f1ea3f
leandroltfx <leandroltfbr@hotmail.com>
2025-10-02 20:46:35 -0300

    inicia página de cadastro de usuário

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'register',
+        loadComponent: () => import('./pages/user-registration/user-registration.component').then(m => m.UserRegistrationComponent)
+    }
+];
