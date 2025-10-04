6fb87b20800626639bde27b9115f697e4273d7cc
leandroltfx <leandroltfbr@hotmail.com>
2025-10-03 21:38:25 -0300

    inicia página de login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,15 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
+        path: 'login',
+        loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent)
+    },
+    {
         path: 'register',
         loadComponent: () => import('./pages/user-registration/user-registration.component').then(m => m.UserRegistrationComponent)
+    },
+    {
+        path: '**',
+        redirectTo: 'login'
     }
 ];
